37b907081de2f830a0c6485203a34b714310855f
alu10486050 <alunac6050@ieselcaminas.org>
2023-01-17 18:47:59 +0100

    Initial commit

---
+++ src/main/java/com/example/producto2/seeds/Seeds.java
@@ -0,0 +1,22 @@
+package com.example.producto2.seeds;
+
+import com.example.producto2.model.Menu;
+import com.example.producto2.repository.MenuRepository;
+
+public class Seeds {
+    private MenuRepository menuRepository;
+
+    public Seeds(MenuRepository menuRepository) {this.menuRepository = menuRepository; }
+
+    public void generateSeeds(){
+
+        Menu menuLibanes = new Menu("Libanes", 30.5);
+        Menu menuVeggie = new Menu("Veggie", 20);
+        Menu menuBurguer = new Menu("Libanes", 25);
+
+        menuRepository.save(menuLibanes);
+        menuRepository.save(menuVeggie);
+        menuRepository.save(menuBurguer);
+    }
+
+}
